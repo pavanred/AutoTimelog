f186fb1647d8182db8be84ab6008208f32f89a0c
pavanred <pavankumar.kh@gmail.com>
2013-08-08 18:15:22 -0500

    activity ddl for current session and lifetime

--- setup.py
+++ setup.py
@@ -4,46 +4,50 @@ Created on Aug 7, 2013
 @author: pavan
 '''
 from pysqlite2 import dbapi2 as sqlite
+import os
 
-class Setup(object):
-    '''
-    application setup 
-    '''
-    def makedb(self):
+def main():
+    print '***Timelog setup***'
+    
+    try:
+        if not os.path.exists('data'):
+            os.makedirs('data')
+            
+        print 'creating sqlite database...'
         connection = sqlite.connect('data/timelog.sqlite')
         cursor = connection.cursor()
         
+        print 'executing ddls'
         #appliction list
         cursor.execute('CREATE TABLE application ' +
-                       '(id INTEGER NOT NULL, ' +
-                       'name VARCHAR(50), ' +
-                       'class VARCHAR(50), ' +
-                       ' PRIMARY KEY (id));')
+                        '(id INTEGER NOT NULL, ' +
+                        'name VARCHAR(50), ' +
+                        'class VARCHAR(50), ' +
+                        ' PRIMARY KEY (id));')
         
-        #capture lookup - daily / weekly / montly 
-        cursor.execute('CREATE TABLE capturetype ' + 
-                       '(id INTEGER NOT NULL, ' +
-                       'type VARCHAR(10), ' +
-                       'PRIMARY KEY (id));')
-        
-        #log 
-        cursor.execute('CREATE TABLE log ' +
-                       '(appid INTEGER NOT NULL, ' +
-                       'duration REAL NOT NULL, ' +
-                       'capturetype INTEGER NOT NULL, '+
-                       'FOREIGN KEY(appid) REFERENCES application(id), ' +
-                       'FOREIGN KEY(capturetype) REFERENCES capturetype(id));')
-        
-        #configuration values
-        cursor.execute('CREATE TABLE config ' +
-                       '(key VARCHAR(50) NOT NULL, ' +
-                       'value VARCHAR(50));')
+        #activities of current session 
+        cursor.execute('CREATE TABLE activity_session ' +
+                        '(appid INTEGER NOT NULL, ' +
+                        'hours INTEGER NOT NULL, ' +
+                        'minutes INTEGER NOT NULL, ' +
+                        'FOREIGN KEY(appid) REFERENCES application(id));')
+            
+        #activity lifetime
+        cursor.execute('CREATE TABLE activity_lifetime ' +
+                        '(appid INTEGER NOT NULL, ' +
+                        'days INTEGER NOT NULL, ' +
+                        'hours INTEGER NOT NULL, ' +
+                        'minutes INTEGER NOT NULL, ' +
+                        'FOREIGN KEY(appid) REFERENCES application(id));')
         
         connection.commit()
+            
+        print 'setup complete'
+    
+    except:
+        raise
 
-    def __init__(self):
-        '''
-        constructor
-        '''
+if __name__ == "__main__":
+    main()
 
         
